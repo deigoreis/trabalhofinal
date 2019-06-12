402b8c8353d7c45e025bec3dbdadc2949a7391cd
Diego Reis Silveira <diego_26_10_1997@hotmail.com>
2019-06-12 15:50:02 -0300

    programa principal

--- distribuidora/ConsoleApp1/Program.cs
+++ distribuidora/ConsoleApp1/Program.cs
@@ -10,6 +10,120 @@ namespace ConsoleApp1
 	{
 		static void Main()
 		{
+			string no;
+			int op, d, m, a;
+			List<cerveja> lc = new List<cerveja>();
+			List<venda> lv = new List<venda>();
+			cerveja c;
+			venda v;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("               MENU");
+				Console.WriteLine("----------------------------------");
+				Console.WriteLine("1-Cadastrar cerveja");
+				Console.WriteLine("2-Mostrar cervejas");
+				Console.WriteLine("3-Fazer uma venda");
+				Console.WriteLine("4-Mostrar vendas");
+				Console.WriteLine("5-Sair");
+				Console.WriteLine("----------------------------------");
+				Console.Write("Opcao: ");
+				op = int.Parse(Console.ReadLine());
+
+				switch (op)
+				{
+					case 1:
+						Console.Clear();
+						Console.WriteLine("Cadastro cerveja!");
+						c = null;
+						string ma;
+						Console.Write("Marca: ");
+						ma = Console.ReadLine();
+						c = new cerveja(ma);
+						Console.Write("Nome do rotulo: ");
+						c.Nome = Console.ReadLine();
+						Console.Write("Preco: ");
+						c.Preco = double.Parse(Console.ReadLine());
+						Console.WriteLine("Data de validade");
+						Console.Write("Dia: ");
+						d = int.Parse(Console.ReadLine());
+						Console.Write("Mes: ");
+						m = int.Parse(Console.ReadLine());
+						Console.Write("Ano: ");
+						a = int.Parse(Console.ReadLine());
+						c.setdataval(d, m, a);
+						lc.Add(c);
+
+
+						Console.ReadKey();
+						break;
+					case 2:
+						Console.Clear();
+						Console.WriteLine("Cervejas!");
+						foreach (cerveja item in lc)
+						{
+							Console.WriteLine(item.mostra());
+						}
+						Console.ReadKey();
+						break;
+
+					case 3:
+						Console.Clear();
+						Console.WriteLine("Vendas!");
+						int qt;
+						char resp;
+						v = new venda();
+						Console.Write("Codigo da venda: ");
+						v.Codv = int.Parse(Console.ReadLine());
+						Console.WriteLine("Data da venda");
+						Console.Write("Dia: ");
+						d = int.Parse(Console.ReadLine());
+						Console.Write("Mes: ");
+						m = int.Parse(Console.ReadLine());
+						Console.Write("Ano: ");
+						a = int.Parse(Console.ReadLine());
+						v.setdatavenda(d, m, a);
+						Console.WriteLine("Cervejas ");
+						do
+						{
+							Console.Write("Rotulo: ");
+							no = Console.ReadLine();
+							Console.Write("Quantidade: ");
+							qt = int.Parse(Console.ReadLine());
+							foreach (cerveja item in lc)
+							{
+								if (no == item.Nome)
+								{
+									do
+									{
+										v.addprod(item);
+										qt--;
+									} while (qt != 0);
+								}
+							}
+							Console.Write("Deseja adicionar outra? (s/n): ");
+							resp = char.Parse(Console.ReadLine());
+						} while (resp != 'n');
+						Console.ReadKey();
+						break;
+					case 4:
+						Console.Clear();
+						Console.WriteLine("Controle de venda!");
+						foreach (venda item in lv)
+						{
+							Console.WriteLine(item.mostra());
+						}
+						Console.ReadKey();
+						break;
+					case 5:
+						break;
+					default:
+						Console.WriteLine("Opcao invalida!");
+						break;
+				}
+
+			} while (op != 5);
+			Console.ReadKey();
 		}
 	}
 }
