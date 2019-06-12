0fbc5aa37554f8bac4740f6bb75d21bee6d57c25
Diego Reis Silveira <diego_26_10_1997@hotmail.com>
2019-06-12 15:48:14 -0300

    distribuidora

---
+++ distribuidora/ConsoleApp1/alimento.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class produto
+	{
+		protected string marca;
+		private tdata dataval;
+
+		public produto(string m)
+		{
+			marca = m;
+			dataval = new tdata();
+		}
+
+		public string setdataval(int d, int m, int a)
+		{
+			string aux = "";
+			if (dataval.gravar(d, m, a))
+				aux = "data valida";
+			else
+				aux = "data invalida";
+
+			return aux;
+		}
+
+		public string getdataval()
+		{
+
+			return dataval.mostra();
+		}
+
+		virtual public string mostra()
+		{
+			return "Nome: " + marca + " ,Data: " + getdataval();
+		}
+
+
+
+	}
+}
